f15a1731d4777d5c28cdd775c63946ece219fc46
alex-duchnowski-instalily <alex.duchnowski@instalily.ai>
2026-05-27 18:34:18 -0400

    feat: add oil (and switch back to tokyonight theme)

--- init.lua
+++ init.lua
@@ -762,36 +762,36 @@ require('lazy').setup({
         },
     },
 
-    -- { -- You can easily change to a different colorscheme.
-    --     -- Change the name of the colorscheme plugin below, and then
-    --     -- change the command in the config to whatever the name of that colorscheme is.
-    --     --
-    --     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    --     'folke/tokyonight.nvim',
-    --     priority = 1000, -- Make sure to load this before all the other start plugins.
-    --     config = function()
-    --         ---@diagnostic disable-next-line: missing-fields
-    --         require('tokyonight').setup {
-    --             styles = {
-    --                 comments = { italic = false }, -- Disable italics in comments
-    --             },
-    --         }
-    --
-    --         -- Load the colorscheme here.
-    --         -- Like many other themes, this one has different styles, and you could load
-    --         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    --         vim.cmd.colorscheme 'tokyonight-night'
-    --     end,
-    -- },
-
-    {
-        "Tsuzat/NeoSolarized.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+    { -- You can easily change to a different colorscheme.
+        -- Change the name of the colorscheme plugin below, and then
+        -- change the command in the config to whatever the name of that colorscheme is.
+        --
+        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+        'folke/tokyonight.nvim',
+        priority = 1000, -- Make sure to load this before all the other start plugins.
         config = function()
-            vim.cmd [[ colorscheme NeoSolarized ]]
-        end
+            ---@diagnostic disable-next-line: missing-fields
+            require('tokyonight').setup {
+                styles = {
+                    comments = { italic = false }, -- Disable italics in comments
+                },
+            }
+
+            -- Load the colorscheme here.
+            -- Like many other themes, this one has different styles, and you could load
+            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+            vim.cmd.colorscheme 'tokyonight-night'
+        end,
     },
+
+    -- {
+    --     "Tsuzat/NeoSolarized.nvim",
+    --     lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    --     priority = 1000, -- make sure to load this before all the other start plugins
+    --     config = function()
+    --         vim.cmd [[ colorscheme NeoSolarized ]]
+    --     end
+    -- },
 
     { -- Highlight todo, notes, etc in comments
         'folke/todo-comments.nvim',
@@ -902,6 +902,24 @@ require('lazy').setup({
             })
         end,
     },
+
+    {
+      'stevearc/oil.nvim',
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {
+        view_options = {
+                show_hidden = true,
+            }
+        },
+      -- Optional dependencies
+      dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      lazy = false,
+    },
+
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
