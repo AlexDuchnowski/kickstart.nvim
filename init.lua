89502521b9e62af39ea1700509d900f60a418e9a
alex-duchnowski-instalily <alex.duchnowski@instalily.ai>
2026-05-27 17:37:33 -0400

    style: change theme to solarized

--- init.lua
+++ init.lua
@@ -661,7 +661,7 @@ require('lazy').setup({
             formatters_by_ft = {
                 -- rust = { 'rustfmt' },
                 -- Conform can also run multiple formatters sequentially
-                -- python = { "isort", "black" },
+                python = { "isort", "black" },
                 --
                 -- You can use 'stop_after_first' to run the first available formatter from the list
                 -- javascript = { "prettierd", "prettier", stop_after_first = true },
@@ -762,30 +762,38 @@ require('lazy').setup({
         },
     },
 
-    { -- You can easily change to a different colorscheme.
-        -- Change the name of the colorscheme plugin below, and then
-        -- change the command in the config to whatever the name of that colorscheme is.
-        --
-        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'folke/tokyonight.nvim',
-        priority = 1000, -- Make sure to load this before all the other start plugins.
-        config = function()
-            ---@diagnostic disable-next-line: missing-fields
-            require('tokyonight').setup {
-                styles = {
-                    comments = { italic = false }, -- Disable italics in comments
-                },
-            }
+    -- { -- You can easily change to a different colorscheme.
+    --     -- Change the name of the colorscheme plugin below, and then
+    --     -- change the command in the config to whatever the name of that colorscheme is.
+    --     --
+    --     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    --     'folke/tokyonight.nvim',
+    --     priority = 1000, -- Make sure to load this before all the other start plugins.
+    --     config = function()
+    --         ---@diagnostic disable-next-line: missing-fields
+    --         require('tokyonight').setup {
+    --             styles = {
+    --                 comments = { italic = false }, -- Disable italics in comments
+    --             },
+    --         }
+    --
+    --         -- Load the colorscheme here.
+    --         -- Like many other themes, this one has different styles, and you could load
+    --         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    --         vim.cmd.colorscheme 'tokyonight-night'
+    --     end,
+    -- },
 
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
-        end,
+    {
+        "Tsuzat/NeoSolarized.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            vim.cmd [[ colorscheme NeoSolarized ]]
+        end
     },
 
-    -- Highlight todo, notes, etc in comments
-    {
+    { -- Highlight todo, notes, etc in comments
         'folke/todo-comments.nvim',
         event = 'VimEnter',
         dependencies = { 'nvim-lua/plenary.nvim' },
