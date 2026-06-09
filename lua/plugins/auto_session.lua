a49d0875d7418b933a352e02b10ac17a1ab32ec2
alex-duchnowski-instalily <alex.duchnowski@instalily.ai>
2026-06-09 18:37:32 -0400

    refactor: change some keymaps for telescope and autosession

--- lua/plugins/auto_session.lua
+++ lua/plugins/auto_session.lua
@@ -1,12 +1,17 @@
 return {
   'rmagatti/auto-session',
   lazy = false, -- auto-session must load at startup to restore the session
+  keys = {
+    -- Will use Telescope if installed or a vim.ui.select picker otherwise
+    { '<leader>ss', '<cmd>AutoSession search<CR>', desc = 'Session search' },
+    -- { '<leader>as', '<cmd>AutoSession save<CR>', desc = 'Save session' },
+    { '<leader>ta', '<cmd>AutoSession toggle<CR>', desc = '[T]oggle [A]utosession Autosave' },
+  },
 
   ---enables autocomplete for opts
   ---@module 'auto-session'
   ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
   },
 }
